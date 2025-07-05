b0342194664b41964e4c08ae0963e5b43e62d64c
Muhammad Yasir Malkani <muhammadyasirmalkani1@gmail.com>
2025-07-05 23:55:24 +0500

    Delete app directory

--- app/chat/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react"
-
-export default function ChatLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
-  return <div className="cosmic-bg pt-24">{children}</div>
-}
